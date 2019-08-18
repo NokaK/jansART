f78ecd6adc3bb1401c721c9cf6523daf00614706
NokaK <nokakipiani93@gmail.com>
2019-08-18 15:12:37 +0400

    ADD CONTEN

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# jansART
+# --ART
---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#img1').click(function(){
+		$('#page2').slideToggle("slow", function(){
+		    if($('#page2').is(":visible")){
+		        $("html, body").animate({scrollTop: $('#page2').offset().top},500);
+		    }
+		});
+	});
+})
